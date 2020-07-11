6489dc8d67659738664c39f641669709e886cd82
jslee <lighthouse2.dev@gmail.com>
2020-07-12 02:46:30 +0900

    해설 추가

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,13 @@ import React, { useState } from "react";
 
 // 해설
 // handleAlertClick() 함수는 여러번 호출되지만(랜더링마다 한 번씩),
-// 각각의 랜더링에서 함수 안의 count 값은 상수이자 독립적인 값(특정 랜더링 시의 상태)으로 존재함
+// 각각의 랜더링에서 함수 안의 count 값은 상수이자 독립적인 값(특정 랜더링 시의 상태)으로 존재함.
+
+// 특정 랜더링 시 그 내부에서 props와 state는 영원히 같은 상태로 유지.
+// props와 state가 랜더링으로부터 분리되어 있다면, 이를 사용하는 어떠한 값(이벤트 핸들러를 포함하여)도 분리되어 있는 것.
+// 이들도 마찬가지로 특정 랜더링에 “속해 있다”
+// 따라서 이벤트 핸들러 내부의 비동기 함수라 할지라도 같은 count 값을 “보게” 되는 것.
+// https://rinae.dev/posts/a-complete-guide-to-useeffect-ko
 
 export default function App() {
   const [count, setCount] = useState(0);

2960c440349871179d7329099a4f7084f7978170
jslee <lighthouse2.dev@gmail.com>
2020-07-12 02:48:38 +0900

    링크 수정

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ import React, { useState } from "react";
 // props와 state가 랜더링으로부터 분리되어 있다면, 이를 사용하는 어떠한 값(이벤트 핸들러를 포함하여)도 분리되어 있는 것.
 // 이들도 마찬가지로 특정 랜더링에 “속해 있다”
 // 따라서 이벤트 핸들러 내부의 비동기 함수라 할지라도 같은 count 값을 “보게” 되는 것.
-// https://rinae.dev/posts/a-complete-guide-to-useeffect-ko
+// https://rinae.dev/posts/a-complete-guide-to-useeffect-ko#%EB%AA%A8%EB%93%A0-%EB%9E%9C%EB%8D%94%EB%A7%81%EC%9D%80-%EA%B3%A0%EC%9C%A0%EC%9D%98-%EC%9D%B4%EB%B2%A4%ED%8A%B8-%ED%95%B8%EB%93%A4%EB%9F%AC%EB%A5%BC-%EA%B0%80%EC%A7%84%EB%8B%A4
 
 export default function App() {
   const [count, setCount] = useState(0);
